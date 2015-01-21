94edd3539699e83b40fd4fbce1aed37d837b0dcc
Camille Reynders <camille.reynders@gmail.com>
2015-01-21 13:43:33 +0100

    Swap bad grunt modules and style code

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
 /*global describe, it */
-'use strict';
-var assert = require('assert');
-var dPacpluginParser = require('../');
+"use strict";
+var assert = require( "assert" );
+var dPacpluginParser = require( "../" );
 
-describe('d-pacplugin-parser node module', function () {
-  it('must have at least one test', function () {
+describe( "d-pacplugin-parser node module", function(){
+  it( "must have at least one test", function(){
     dPacpluginParser();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
-  });
-});
+    assert( false, "I was too lazy to write any tests. Shame on me." );
+  } );
+} );
