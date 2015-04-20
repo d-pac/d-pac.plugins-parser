1201c6a7517eddad690a6756a2f34407576ef322
Camille Reynders <camille.reynders@gmail.com>
2015-04-20 12:38:33 +0200

    Small fixes

--- cli.js
+++ cli.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 "use strict";
 var meow = require( "meow" );
+var _ = require( "lodash" );
 var parser = require( "./lib/pluginsParser" );
 
 var cli = meow( {
   help : [
     "Usage",
-    "  d-pac-plugins <directory> --manifest=<file>",
+    "  d-pac-plugins [<directory>] [--manifest=<file>] [--framework=<semver>] [--verbose] [--version]",
     "",
     "Example (1)",
     "  $ d-pac-plugins",
@@ -15,11 +16,18 @@ var cli = meow( {
     "Example (3)",
     "  $ d-pac-plugins --manifest=dependencies.json"
   ].join( "\n" )
+}, {
+  alias : {
+    "v" : "verbose",
+    "m" : "manifest",
+    "f" : "framework",
+    "d" : "dir"
+  }
 } );
 
-parser( {
-  manifest : cli.flags.manifest,
-  dir      : cli.input[ 0 ],
-  verbose  : cli.flags.verbose,
-  cli      : true
-} );
+var opts = _.defaults( {
+  cli : true,
+  dir : cli.input[ 0 ]
+}, cli.flags );
+
+parser(opts);
